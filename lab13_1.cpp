a3660179b0178c036910ac260170c7db70617703
gusntc <Natacha_rungbanpant@cmu.ac.th>
2021-12-24 13:53:34 +0700

    Lab13_1Edit1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
-    cout << fibonacci(x);
+    cout << fibonacci(15);
     return 0; 
+}
+
+int fibonacci(int y){
+    if(y <= 1){
+        return y;
+    }else{
+        return fibonacci(y-1)+fibonacci(y-2);
+    }
+     
 }
