86e094168e0d41869e8e6c0606334b65e5a5160f
gusntc <Natacha_rungbanpant@cmu.ac.th>
2021-12-24 14:17:25 +0700

    Lab13_2Edit

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int x,int y);
+
 int main(){
     int x, y;
     cin >> x >> y;
-    cout << gcd(x,y);
+    cout << gcd(12540,4580);
     return 0; 
+}
+
+int gcd(int x,int y){
+    if(x % y == 0){
+        return y;
+    }else{
+        return gcd(y, x%y);
+    }
 }
