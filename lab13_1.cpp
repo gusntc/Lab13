fa10e39b34987d5287c2897d78d25c559e9c3964
gusntc <Natacha_rungbanpant@cmu.ac.th>
2021-12-24 14:02:31 +0700

    Lab13_1Edit2

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,16 +1,19 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
 
-int fibonacci(int);
+long int fibonacci(long int);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(15);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
-int fibonacci(int y){
+long int fibonacci(long int y){
     if(y <= 1){
         return y;
     }else{
